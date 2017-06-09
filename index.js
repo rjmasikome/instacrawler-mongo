848a547f3fb73ee0de78c4173314fef29a2ca695
Rian Josua Masikome <rjmasikome@gmail.com>
2017-06-09 00:23:36 +0000

    Remove token

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 var getInsta = require("./lib/getInsta");
 var handler = require("./lib/handler");
 
-try {
-  var hashtag = process.argv[2] || "blahblahblahblahhhh" || "kotamanado";
-  var token =  process.argv[3] || "300415773.ba4c844.8794e0a44e7c4bba8fe559dd995737c0";
-  var url =  `https://api.instagram.com/v1/tags/${hashtag}/media/recent?access_token=${token}`;
+var hashtag = process.argv[2];
+var token =  process.argv[3];
+var url =  `https://api.instagram.com/v1/tags/${hashtag}/media/recent?access_token=${token}`;
 
+if (url && token) {
   getInsta(url,handler);
 }
-catch(err) {
+else {
   console.log("No hashtag and/or access_token provided");
   console.log("Example usage: ");
   console.log("node . <HASHTAG> <ACCESS-TOKEN>");
